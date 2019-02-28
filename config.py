38d4cefaaf2b02f6c09d0cd17de3659fb12d9f9d
Muji Girl <miki@fyi.tokyo>
2019-02-28 12:06:29 -0800

    Pagination

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['nomuramiki1017@gmail.com']
+
+	POSTS_PER_PAGE = 25
